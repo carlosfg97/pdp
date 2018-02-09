85f765f5ebaaf88c0fd71acc347ef90f787e4799
Carlos Fabbri <carlosfg.97@gmail.com>
2018-02-09 10:54:05 -0500

    added files related to a tutorial

---
+++ VENTAS2017.sql
@@ -0,0 +1,50 @@
+use DATALAB
+
+select PKEbelista, AnioCampana,  
+sum([RealUUVendidas]) as RealUUVendidas,
+sum([RealUUFaltantes]) as RealUUFaltantes,
+sum([RealUUDevueltas]) as [RealUUDevueltas],
+sum(RealUUAnuladas) as RealUUAnuladas,
+sum(RealVtaMNNeto) as RealVtaMNNeto,
+sum(RealVtaMNFaltNeto) as RealVtaMNFaltNeto,
+sum(RealDevMNNeto) as RealDevMNNeto,
+sum(RealAnulMNNeto) as RealAnulMNNeto
+INTO ventas2017
+from DWH_ANALITICO.DBO.DWH_FVTAPROEBECAM
+where CodPais = 'PE' and AnioCampana = AnioCampanaRef and AnioCampana > 201700 and AnioCampana < 201800
+group by PKEbelista, AnioCampana
+
+select * 
+from DWH_ANALITICO.DBO.DWH_FSTAEBECAM
+
+drop table #listaPk
+select distinct PKEbelista into #ListaPk from Ventas2017
+--272614 rows
+
+
+drop table #MuestraDePk
+SELECT * into #MuestraDePk FROM #listaPk
+WHERE (ABS(CAST((BINARY_CHECKSUM(PKEbelista, NEWID())) as int))% 100) < 10
+--27233 rows (10%)
+
+drop table #muestraperu
+select a.* into #muestraperu from ventas2017 a
+inner join #MuestraDePk b on a.PKEbelista=b.PKEbelista
+order by a.PKEbelista, AnioCampana
+--217746 rows
+
+select PKEbelista, AnioCampana, RealVtaMNNeto 
+from #muestraperu
+WHERE AnioCampana = 201701
+
+select AnioCampana, sum(realVtaMNNeto) as total_ventas, COUNT(realvtaMNnETO) as total_pedidos
+into #pdp
+from #muestraperu
+group by AnioCampana
+order by AnioCampana
+
+select *, total_ventas/total_pedidos as p$p
+into pdp
+from #pdp
+--podria ser que empiezar a disminuir su venta cuando entran nuevas consultoras cerca a ella
+
