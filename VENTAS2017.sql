85a0c760357cc61ae95a68deb3821d8db83e7acd
Carlos Fabbri <carlosfg.97@gmail.com>
2018-02-13 14:36:13 -0500

    improved sampling method to include consultants that didnt sell

--- VENTAS2017.sql
+++ VENTAS2017.sql
@@ -1,7 +1,33 @@
 use DATALAB
 
+
+
+--obteniendo muestra de PK
+drop table #ListaPk
+SELECT distinct PKEbelista, CodPais into #ListaPk
+FROM [DWH_ANALITICO].[dbo].[DWH_FSTAEBECAM]
+where CodPais = 'PE' AND AnioCampana = '201609'
+
+  DROP TABLE #AniosCamps
+  select distinct AnioCampana into #AniosCamps
+   from [DWH_ANALITICO].[dbo].[DWH_FSTAEBECAM]
+
+drop table #ListaPkEbe
+SELECT * into #ListaPkEbe
+FROM  #ListaPk CROSS JOIN #AniosCamps
+order by PKEbelista
+
+select * from #ListaPkEbe
+
+
+drop table #MuestraDePk
+SELECT * into #MuestraDePk FROM #ListaPkEbe
+WHERE (ABS(CAST((BINARY_CHECKSUM(PKEbelista, NEWID())) as int))% 100) < 10
+--(4458831 row(s) affected)
+
+
 drop table ventas2017
-select PKEbelista, AnioCampana,  
+select a.PKEbelista, a.AnioCampana,  
 sum([RealUUVendidas]) as RealUUVendidas,
 sum([RealUUFaltantes]) as RealUUFaltantes,
 sum([RealUUDevueltas]) as [RealUUDevueltas],
@@ -11,37 +37,16 @@ sum(RealVtaMNFaltNeto) as RealVtaMNFaltNeto,
 sum(RealDevMNNeto) as RealDevMNNeto,
 sum(RealAnulMNNeto) as RealAnulMNNeto
 INTO ventas2017
-from DWH_ANALITICO.DBO.DWH_FVTAPROEBECAM
-where CodPais = 'PE' and AnioCampana = AnioCampanaRef and AnioCampana > 201700 and AnioCampana < 201800
-group by PKEbelista, AnioCampana
+from #MuestraDePk a left join DWH_ANALITICO.DBO.DWH_FVTAPROEBECAM b on a.PKEbelista = b.PKEbelista and a.CodPais = b.CodPais and a.AnioCampana = b.AnioCampana
+group by a.PKEbelista, a.AnioCampana
+order by a.PKEbelista, a.AnioCampana
 
 select * from ventas2017
 
-
-drop table #listaPk
-select distinct PKEbelista into #ListaPk from Ventas2017
---272614 rows
-
-
-drop table #MuestraDePk
-SELECT * into #MuestraDePk FROM #listaPk
-WHERE (ABS(CAST((BINARY_CHECKSUM(PKEbelista, NEWID())) as int))% 100) < 10
---27233 rows (10%)
-
-drop table #muestraperu
-select a.* into #muestraperu from ventas2017 a
-inner join #MuestraDePk b on a.PKEbelista=b.PKEbelista
-order by a.PKEbelista, AnioCampana
---217746 rows
-select * from #muestraperu
-
-select PKEbelista, AnioCampana, RealVtaMNNeto 
-from #muestraperu
-WHERE AnioCampana = 201701
-
+drop table #pdp
 select AnioCampana, sum(realVtaMNNeto) as total_ventas, COUNT(realvtaMNnETO) as total_pedidos
 into #pdp
-from #muestraperu
+from ventas2017
 group by AnioCampana
 order by AnioCampana
 
