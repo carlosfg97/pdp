f458ae9aed211b7317231353be5c0569e6260309
Carlos Fabbri <carlosfg.97@gmail.com>
2018-02-13 17:46:45 -0500

    muestreo de todo peru

--- VENTAS2017.sql
+++ VENTAS2017.sql
@@ -7,24 +7,25 @@ drop table #ListaPk
 SELECT distinct PKEbelista, CodPais into #ListaPk
 FROM [DWH_ANALITICO].[dbo].[DWH_FSTAEBECAM]
 where CodPais = 'PE' AND AnioCampana = '201609'
-
-  DROP TABLE #AniosCamps
-  select distinct AnioCampana into #AniosCamps
-   from [DWH_ANALITICO].[dbo].[DWH_FSTAEBECAM]
-
-drop table #ListaPkEbe
-SELECT * into #ListaPkEbe
-FROM  #ListaPk CROSS JOIN #AniosCamps
-order by PKEbelista
-
-select * from #ListaPkEbe
+--(1439202 row(s) affected)
 
 
 drop table #MuestraDePk
-SELECT * into #MuestraDePk FROM #ListaPkEbe
+SELECT * into #MuestraDePk FROM #ListaPk
 WHERE (ABS(CAST((BINARY_CHECKSUM(PKEbelista, NEWID())) as int))% 100) < 10
---(4458831 row(s) affected)
 
+select * from #MuestraDePk
+--143796
+
+DROP TABLE #AniosCamps
+select distinct AnioCampana into #AniosCamps
+from [DWH_ANALITICO].[dbo].[DWH_FSTAEBECAM]
+
+drop table #ListaPkEbe
+SELECT * into #ListaPkEbe
+FROM  #MuestraDePk CROSS JOIN #AniosCamps
+order by PKEbelista
+--((4457676 row(s) affected)
 
 drop table ventas2017
 select a.PKEbelista, a.AnioCampana,  
@@ -37,11 +38,13 @@ sum(RealVtaMNFaltNeto) as RealVtaMNFaltNeto,
 sum(RealDevMNNeto) as RealDevMNNeto,
 sum(RealAnulMNNeto) as RealAnulMNNeto
 INTO ventas2017
-from #MuestraDePk a left join DWH_ANALITICO.DBO.DWH_FVTAPROEBECAM b on a.PKEbelista = b.PKEbelista and a.CodPais = b.CodPais and a.AnioCampana = b.AnioCampana
+from #ListaPkEbe a left join DWH_ANALITICO.DBO.DWH_FVTAPROEBECAM b on a.PKEbelista = b.PKEbelista and a.CodPais = b.CodPais and a.AnioCampana = b.AnioCampana
 group by a.PKEbelista, a.AnioCampana
 order by a.PKEbelista, a.AnioCampana
 
 select * from ventas2017
+
+select distinct Pkebelista from ventas2017
 
 drop table #pdp
 select AnioCampana, sum(realVtaMNNeto) as total_ventas, COUNT(realvtaMNnETO) as total_pedidos
